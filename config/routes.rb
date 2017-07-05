d62c37a5eb26ea57664c993b91ffbd2773621965
Isaac Tsai <isaac@adup.com.tw>
2017-07-05 14:14:29 +0800

    root to routines

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :routines
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "routines#index"
 end
