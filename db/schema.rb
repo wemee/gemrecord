947b2b241f6a6b0cb8a03e1633f21e86a1706a31
Isaac Tsai <isaac@adup.com.tw>
2017-07-06 17:04:19 +0800

    db seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706083940) do
+ActiveRecord::Schema.define(version: 20170704042325) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 20170706083940) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_exercises_on_category_id"
     t.index ["user_id"], name: "index_exercises_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create!(
+  email: "u1@workout.com",
+  password: "111111",
+  password_confirmation: "111111")
+u2 = User.create!(email: "u2@workout.com",
+  password: "111111",
+  password_confirmation: "111111")
+
+categories = Category.create(
+  [{name: "胸", user: u1}, {name: "背", user: u1}, {name: "腿", user: u1}, {name: "其他", user: u1}]
+)
+exercises = Exercise.create([
+  {name: "深蹲", user: u1, category: categories[2]},
+  {name: "硬舉", user: u1, category: categories[2]},
+  {name: "臥推", user: u1, category: categories[0]}
+])
