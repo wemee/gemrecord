e353cfd676e49cb68bc842e969b89fd5b4eb4984
Isaac Tsai <isaac@adup.com.tw>
2017-07-07 15:43:58 +0800

    新帳號設定預設運動項目

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,28 @@ class User < ApplicationRecord
 
   private
   def set_defuat_exercises
+    cate1 = Category.find_or_create_by(name: '胸')
+    cate1.user = self
+    exercise1 = Exercise.find_or_create_by(name: '臥推')
+    exercise1.user =  self
+    cate1.exercises << exercise1
+    cate1.save
 
+    cate2 = Category.find_or_create_by(name: '背')
+    cate2.user = self
+    exercise2 = Exercise.find_or_create_by(name: '槓鈴划船')
+    exercise2.user =  self
+    cate2.exercises << exercise2
+    cate2.save
+
+    cate3 = Category.find_or_create_by(name: '腿')
+    cate3.user = self
+    exercise3 = Exercise.find_or_create_by(name: '深蹲')
+    exercise3.user =  self
+    cate3.exercises << exercise3
+    exercise4 = Exercise.find_or_create_by(name: '硬舉')
+    exercise4.user =  self
+    cate3.exercises << exercise4
+    cate3.save
   end
 end
